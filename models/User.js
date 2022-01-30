1f133594b61d9d67db00aa4a58923945692975ea
Keemperor <jacobotau@hotmail.com>
2022-01-30 00:44:20 -0500

    password hashing impplemented and login validation created

--- models/User.js
+++ models/User.js
@@ -3,7 +3,12 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+};
 
 // define table columns and configuration
 User.init(
@@ -35,6 +40,7 @@ User.init(
       validate: {
         isEmail: true
       }
+      
     },
     // define a password column
     password: {
@@ -44,7 +50,9 @@ User.init(
         // this means the password must be at least four characters long
         len: [4]
       }
+      
     }
+    
   },
   
   {
@@ -74,6 +82,9 @@ User.init(
     // make it so our model name stays lowercase in the database
     modelName: 'user'
   }
+  
 );
+
+
 
 module.exports = User;
