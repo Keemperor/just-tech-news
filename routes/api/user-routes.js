8f5fd333554a534eb0a567aa38f280cd96428dab
Keemperor <jacobotau@hotmail.com>
2022-02-06 00:57:36 -0500

    added votes and cleaned up code

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Vote } = require("../../models");
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -16,11 +16,24 @@ router.get('/', (req, res) => {
 
 // GET /api/users/1
 router.get('/:id', (req, res) => {
-  User.findOne({
-    attributes: { exclude: ['password'] },
+  User.findOne({  
     where: {
       id: req.params.id
-    }
+    },
+    attributes: { exclude: ['password'] },
+    include: [
+      {
+        model: Post,
+        attributes: ['id', 'title', 'post_url', 'created_at']
+      },
+      {
+        model: Post,
+        attributes: ['title'],
+        through: Vote,
+        as: 'voted_posts'
+      }
+    ]
+
   })
     .then(dbUserData => {
       if (!dbUserData) {
